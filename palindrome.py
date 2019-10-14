ad5cacc91b111dd122389ec8f078982769d370fd
Becky <55168393+lunaxtasy@users.noreply.github.com>
2019-10-14 01:06:50 -0400

    Assignment 4: Step 3

--- palindrome.py
+++ palindrome.py
@@ -4,6 +4,8 @@ Assignment 4: Palindrome checker
 Palindrome checker main module
 """
 
+from collections import deque
+
 def is_palindrome(word):
     """
     This function checks to see if an entered string is a palindrome or not.
@@ -16,7 +18,11 @@ def is_palindrome(word):
     if isinstance(word, str) is False:
         raise ValueError
 
+    #if string isn't empty, determines if entry is palindrome
+
     if not word.strip():
         palin_answer = False
+    elif len(deque(word)) == 1:
+        palin_answer = True
 
     return palin_answer
--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -22,3 +22,10 @@ def test_empty_string():
     """
 
     assert is_palindrome("") is False
+
+def test_a_is_a_palindrome():
+    """
+    `(/1)` `is_palindrome` returns `True` if called with `"a"`.
+    """
+
+    assert is_palindrome("a") is True
